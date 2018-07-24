30ffd69bfe4c9d22293101b2dd3d8174b7c0e905
Florent Jaby <florent.jaby@gmail.com>
2018-07-24 14:53:33 +0200

    Remove force https

--- serve.js
+++ serve.js
@@ -4,15 +4,16 @@ const FastbookAppServer = require('fastboot-app-server')
 const server = new FastbookAppServer({
   distPath: Path.resolve(__dirname, 'dist'),
   gizp: true,
-  beforeMiddleware(app) {
-    app.use((req, res, next) => {
-      if (req.headers['x-forwarded-proto'] === 'https') {
-        return next()
-      } else {
-        return res.redirect(301, `https://${req.hostname}${req.url}`)
-      }
-    })
-  }
+  //beforeMiddleware(app) {
+    //app.use((req, res, next) => {
+      //return next()
+      //if (req.headers['x-forwarded-proto'] === 'https') {
+        //return next()
+      //} else {
+        //return res.redirect(301, `https://${req.hostname}${req.url}`)
+      //}
+    //})
+  //}
 })
 
 server.start()
