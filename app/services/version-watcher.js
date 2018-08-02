77488b0b49c70e72b4fe454e7460c79423bdaa1a
Florent Jaby <florent.jaby@gmail.com>
2018-08-02 11:37:26 +0200

    Fix error when getting new version

--- app/services/version-watcher.js
+++ app/services/version-watcher.js
@@ -15,17 +15,15 @@ export default Service.extend({
   checkVersion: async function () {
     const versionUrl = '/api/version'
     const version = this.get('version')
-    let response
     try {
-      let response = await fetch(versionUrl)
+      const response = await fetch(versionUrl)
+      const { version: currentVersion } = await response.json()
+      if (currentVersion !== version) {
+        clearInterval(this.interval)
+        Ember.run(() => this.set('outOfDate', true))
+      }
     } catch (error) {
       console.debug('Could not connect, tuff luck')
-      return
-    }
-    const { version: currentVersion } = await response.json()
-    if (currentVersion !== version) {
-      clearInterval(this.interval)
-      Ember.run(() => this.set('outOfDate', true))
     }
   }
 });
