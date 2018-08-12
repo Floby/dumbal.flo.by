9b2b286eac04c380f86ed383ceb2a2ecd9dc5d1c
Florent Jaby <florent.jaby@gmail.com>
2018-08-13 00:29:07 +0200

    Fix on-hold mixin for touch events

--- app/mixins/on-hold.js
+++ app/mixins/on-hold.js
@@ -2,15 +2,24 @@ import Mixin from '@ember/object/mixin';
 import ms from 'ms';
 
 export default Mixin.create({
+  classNames: ['on-hold'],
   onHoldTimeout: null,
   onHoldDelay: '1s',
-  mouseDown () {
+  didInsertElement () {
+    this.$()
+      .on('mousedown', () => this.holdStart() || false)
+      .on('touchstart', () => this.holdStart() || false)
+      .on('mouseup', () => this.holdStop() || false)
+      .on('touchend', () => this.holdStop() || false)
+      .on('touchleave', () => this.holdStop() || false)
+  },
+  holdStart () {
     this.onHoldTimeout = setTimeout(() => {
       this.onHold()
     }, ms(this.get('onHoldDelay')))
   },
 
-  mouseUp () {
+  holdStop () {
     if (this.onHoldTimeout) {
       clearTimeout(this.onHoldTimeout)
       delete this.onHoldTimeout
