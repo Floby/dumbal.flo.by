4ccdea507c3138c39507fe781689a36471df3168
Florent Jaby <florent.jaby@gmail.com>
2018-08-12 20:03:19 +0200

    Display version when holding header

--- app/components/screen-title-content.js
+++ app/components/screen-title-content.js
@@ -1,8 +1,11 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object'
 import { inject }  from '@ember/service'
+import ms from 'ms';
 
 export default Component.extend({
+  tagName: 'header',
+  classNames: ['screen-title'],
   screenTitle: inject(),
   primaryTitle: computed.alias('screenTitle.primaryTitle'),
   secondaryTitle: computed.alias('screenTitle.secondaryTitle'),
@@ -15,6 +18,27 @@ export default Component.extend({
   hasNavigation: computed('navigationBack', 'navigationHome', function () {
     return this.get('navigationBack')  || this.get('navigationHome')
   }),
+
+  holdTimeout: null,
+  holdDelay: '1s',
+  mouseDown () {
+    this.holdTimeout = setTimeout(() => {
+      this.hold()
+    }, ms(this.get('holdDelay')))
+  },
+
+  mouseUp () {
+    if (this.holdTimeout) {
+      clearTimeout(this.holdTimeout)
+      delete this.holdTimeout
+    }
+  },
+
+  hold () {
+    const version = this.get('version')
+    window.alert(`Version courante : ${version}`)
+  },
+
   actions: {
     goBack () {
       Ember.run(() => history.back())
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,6 +5,7 @@ import { computed } from '@ember/object';
 export default Controller.extend({
   versionWatcher: inject(),
   needsRefresh: computed.alias('versionWatcher.outOfDate'),
+  version: computed.alias('versionWatcher.version'),
   actions: {
     refreshWindow () {
       location.reload()
