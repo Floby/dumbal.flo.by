47a5c701d797a547879ca3ecff7a52f6e1c1fe9e
Florent Jaby <florent.jaby@gmail.com>
2018-09-18 14:40:14 +0200

    Add tests for chronological ordering

--- tests/unit/services/game-test.js
+++ tests/unit/services/game-test.js
@@ -1,13 +1,48 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import { createSandbox } from 'sinon'
+import Store from 'store2'
 
 module('Unit | Service | game', function(hooks) {
   setupTest(hooks);
+  const testStore = Store.namespace('test')
+  let sandbox
+  hooks.beforeEach(() => { sandbox = createSandbox() })
+  hooks.afterEach(() => sandbox.restore())
+  hooks.beforeEach(function () {
+    this.owner.register('service:local-store', testStore, { instantiate: false })
+  })
+  hooks.beforeEach(() => testStore.clear())
+  hooks.afterEach(() => testStore.clear())
 
   // Replace this with your real tests.
   test('it exists', function(assert) {
-    let service = this.owner.lookup('service:game');
+    const service = this.owner.lookup('service:game');
     assert.ok(service);
   });
+
+  test('It lists games in descending date order', function (assert) {
+    // Given
+    sandbox.useFakeTimers(Date.now())
+    const service = this.owner.lookup('service:game');
+    const playerNames = ['A', 'B']
+    const expectedOrderNames = [
+      'Newest',
+      'New',
+      'Medium',
+      'Old',
+      'Oldest'
+    ];
+    expectedOrderNames.slice().reverse().forEach((name) => {
+      service.createNewGame(name, playerNames)
+      sandbox.clock.tick('00:05:00')
+    })
+    // When
+    const orderedNames = service.list().mapBy('name')
+    // Then
+    assert.deepEqual(orderedNames, expectedOrderNames)
+  })
+
 });
+
 
