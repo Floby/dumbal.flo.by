eb554073de1130de94895b5e33e55dfd864188ec
Florent Jaby <florent.jaby@gmail.com>
2018-12-13 23:08:03 +0100

    Vibrate when displaying a warning

--- app/services/notify.js
+++ app/services/notify.js
@@ -2,6 +2,11 @@ import Service from '@ember/service';
 import EmberObject from '@ember/object'
 import { run } from '@ember/runloop'
 
+let vibrate = () => false
+if (navigator && typeof navigator.vibrate === 'function') {
+  vibrate = navigator.vibrate.bind(navigator)
+}
+
 export default Service.extend({
   init () {
     this._super(...arguments)
@@ -13,6 +18,7 @@ export default Service.extend({
       content
     })
     this.get('notifications').pushObject(notification)
+    this.vibrateWarn()
     setTimeout(() => {
       this.expireNotification(notification)
     }, 3000)
@@ -23,5 +29,9 @@ export default Service.extend({
     setTimeout(() => {
       run(() => this.get('notifications').removeObject(notification))
     }, 1000)
+  },
+
+  vibrateWarn () {
+    vibrate([50, 30, 50])
   }
 });
